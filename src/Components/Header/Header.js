c3f2d41288257faf26e3ffceaa1d1948aae7650f
Turumbaeva <turumbaeva4@gmail.com>
2024-05-21 13:56:36 +0600

    updated Car Toyota to Cars

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,6 +5,11 @@ function Header() {
     <header>
       <div>
         <span className='logo'>Car Toyota</span>
+        <ul className='nav'>
+          <li>Про нас </li>
+          <li>Контакты</li>
+          <li>Кабинет</li>
+        </ul>
       </div>
       <div className='presentation'></div>
   </header>
