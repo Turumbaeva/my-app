07ef85be42313cb9159f7d0fff5b596c715e7a3f
Turumbaeva <turumbaeva4@gmail.com>
2024-06-02 21:38:40 +0600

    added menu

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
-import Contacts from './Pages/Contact/Contact';
+import Contact from './Pages/Contact/Contact';
 import Order from './Pages/Order/Order';
 import Delivery from './Pages/Delivery/Delivery';
 import Cart from './Pages/Cart/Cart';
@@ -13,29 +13,68 @@ import {
   Routes,
   Route
 } from 'react-router-dom'
+import { createContext, useEffect,useState } from 'react';
+import { onAuthChange,onCategoriesLoad,onOrdersLoad,onProductsLoad } from './firebase';
+export const AppContext = createContext({
+  categories:[],
+  products:[],
+  orders:[],
+  cart:{},
+  setCart:()=>{ },
+  user:null,
+});
 
 
 function App() {
+  const[categories,setCategories] = useState([]);
+  const[product, setProducts] = useState([]);
+  const[orders, setOrders] = useState ([]);
+  const[cart,setCarts] = useState(()=>{
+    return JSON.parse(localStorage.getItem("cart")) ||{};
+  });
+  const[user,setUser] = useState(null);
+  useEffect(()=>{
+    localStorage.setItem("cart", JSON.stringify(cart));
+  },[cart]);
+  useEffect(() => {
+    onCategoriesLoad(setCategories);
+    onProductsLoad(setProducts);
+    onOrdersLoad(setOrders);
+    onAuthChange(user => {
+      if(user){
+        user.isAdmin = user && user. email ==="turumbaeva4@gmail.com";
+      }
+      setUser(user);
+    })
+  },[] );
+
   return (
-    <div className='wrapper'>
-      <Header>
-        <Footer />
-        <main>
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="contacts" element={<Contacts />} />
-              <Route path="order" element={<Order />} />
-              <Route path="delivery" element={<Delivery />} />
-              <Route path="cart" element={<Cart />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
-        </main>
-        <Footer />
-      </Header>
+    <div className='App'>
+
+      <AppContext.Provider value={{ categories, cart, user, orders }} >
+
+        <Router>
+          <Header />
+          <main>
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="contacts" element={<Contact />} />
+                <Route path="order" element={<Order />} />
+                <Route path="delivery" element={<Delivery />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </main>
+          <Footer />
+        </Router>
+
+      </AppContext.Provider>
+
     </div>
+
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,66 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCIBgsom7PA2PHB8c6sVo8NsmvkmvO8mtc",
+    authDomain: "cars-cc35a.firebaseapp.com",
+    projectId: "cars-cc35a",
+    storageBucket: "cars-cc35a.appspot.com",
+    messagingSenderId: "420769273053",
+    appId: "1:420769273053:web:8c1ffcb74bd333625a3734"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+const auth = getAuth(app);
+export const storage = getStorage(app);
+
+export const categoryCollection = collection(db, "categories");
+export const productsCollection = collection(db, "products");
+export const ordersCollection = collection(db, "orders");
+
+const provider = new GoogleAuthProvider();
+export const LogIn = () => signInWithPopup(auth, provider);
+export const LogOut = () => signOut(auth);
+export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
+
+export const onCategoriesLoad = (callback) =>
+  onSnapshot(categoryCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onProductsLoad = (callback) =>
+  onSnapshot(productsCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onOrdersLoad = (callback) =>
+  onSnapshot(ordersCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+
+export const uploadProductPhoto = (file) => {
+  const storageRef = ref(storage, `products/${file.name}`);
+  return uploadBytes(storageRef, file)
+    .then(() => {
+      return getDownloadURL(storageRef);
+    })
+    .catch((error) => {
+      console.log("Failed to upload product photo:", error);
+    });
+};
